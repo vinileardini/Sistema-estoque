12567e0e18a341ce0c4c024cac89907d1bd58568
Vinícius <vinileardini@gmail.com>
2023-12-09 22:29:01 -0300

    inicio tela menu

--- python/app.py
+++ python/app.py
@@ -3,13 +3,25 @@ import tkinter as ttk
 from tkinter.commondialog import Dialog
 
 class app():
-    master=None
-
-    inicio = Frame(master,background='#121212')
-    inicio.pack()
-
-    teste = Label(inicio,text='TESTE')
-    teste.pack()
+    
+    def __init__(self,master=None):
+    
+        main = Frame(master)
+        main.pack()
+        title = Label(main,text='Sistema de gerenciamento de estoque')
+        title.pack(pady=10)
+        selectionMenu = Label(main)
+        selectionMenu.pack()
+        buttonNewItem = Button(selectionMenu,text='Adicionar Item')
+        buttonNewItem.pack(side=LEFT,padx=10)
+        labelSearchItem = Entry(selectionMenu,width=50)
+        labelSearchItem.pack(side=LEFT,padx=10)
+        buttonSearchItem = Button(selectionMenu)
+        buttonSearchItem.pack(side=RIGHT,padx=10)
+        
+        
     
 root = Tk()
+root.config(background='#121212')
+app(root)
 root.mainloop()
