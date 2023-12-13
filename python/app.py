e64ba61e6ceb92e5a173f25209475172af95015c
Vinícius <vinileardini@gmail.com>
2023-12-13 13:54:48 -0300

    configuracoes dropdown menu

--- python/app.py
+++ python/app.py
@@ -6,21 +6,24 @@ class app():
     
     def __init__(self,master=None):
     
-        main = Frame(master)
+        main = Frame(master,background='#121212')
         main.pack()
-        title = Label(main,text='Sistema de gerenciamento de estoque')
+        title = Label(main,text='Sistema de gerenciamento de estoque',background='#121212',foreground='#ffffff')
         title.pack(pady=10)
-        selectionMenu = Label(main)
+        selectionMenu = Label(main,background='#121212')
         selectionMenu.pack()
         selectedOption = StringVar(value="Selecione uma opção")
         options = ["Adicionar Item","Pesquisar Item","Remover Item"]
-        buttonNewItem = OptionMenu(selectionMenu,selectedOption,*options)
-        buttonNewItem.pack(side=LEFT,padx=10)
+        dropdownMenu = OptionMenu(selectionMenu,selectedOption,*options)
+        dropdownMenu.config(bg='#121212',fg='#ffffff',activebackground='#121212',activeforeground='#ffffff')
+        dropdownMenu["menu"].config(bg='#121212',fg='#ffffff',activebackground='#121212',activeforeground='#ffffff')
+        dropdownMenu["highlightthickness"] = 0
+        dropdownMenu.pack(side=LEFT,padx=10)
         labelSearchItem = Entry(selectionMenu,width=50)
         labelSearchItem.pack(side=LEFT,padx=10)
         searchImage = PhotoImage(file='img/lupa.png')
         searchImage = searchImage.subsample(10,10)
-        buttonSearchItem = Button(selectionMenu,image=searchImage)
+        buttonSearchItem = Button(selectionMenu,image=searchImage,background='#121212')
         buttonSearchItem.image = searchImage
         buttonSearchItem.pack(side=RIGHT,padx=10)
         
