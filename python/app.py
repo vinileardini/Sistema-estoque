4810aae415a895724509d9edf27ae29cf6dd57f2
Vinícius <vinileardini@gmail.com>
2023-12-14 13:34:46 -0300

    novas img e alteracao optionmenu para combobox

--- python/app.py
+++ python/app.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import tkinter as ttk
+from tkinter import ttk
 from tkinter.commondialog import Dialog
 
 class app():
@@ -12,17 +12,16 @@ class app():
         title.pack(pady=10)
         selectionMenu = Label(main,background='#121212')
         selectionMenu.pack()
-        selectedOption = StringVar(value="Selecione uma opção")
         options = ["Adicionar Item","Pesquisar Item","Remover Item"]
-        dropdownMenu = OptionMenu(selectionMenu,selectedOption,*options)
-        dropdownMenu.config(bg='#121212',fg='#ffffff',activebackground='#121212',activeforeground='#ffffff')
-        dropdownMenu["menu"].config(bg='#121212',fg='#ffffff',activebackground='#121212',activeforeground='#ffffff')
-        dropdownMenu["highlightthickness"] = 0
+        dropdownMenu = ttk.Combobox(selectionMenu,textvariable='Selecione uma opção')
+        dropdownMenu['values'] = options
+        dropdownMenu['state'] = 'readonly'
+        dropdownMenu.set('Selecione uma opção')
         dropdownMenu.pack(side=LEFT,padx=10)
         labelSearchItem = Entry(selectionMenu,width=50)
         labelSearchItem.pack(side=LEFT,padx=10)
         searchImage = PhotoImage(file='img/lupa.png')
-        searchImage = searchImage.subsample(10,10)
+        searchImage = searchImage.subsample(18,18)
         buttonSearchItem = Button(selectionMenu,image=searchImage,background='#121212')
         buttonSearchItem.image = searchImage
         buttonSearchItem.pack(side=RIGHT,padx=10)
@@ -31,5 +30,6 @@ class app():
     
 root = Tk()
 root.config(background='#121212')
+root.minsize(300,300)
 app(root)
 root.mainloop()
