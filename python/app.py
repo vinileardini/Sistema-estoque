5d6397f3b6d1354e410ee92f269225ab7b0b0434
Vinícius <vinileardini@gmail.com>
2023-12-14 22:28:11 -0300

    implementação de apresentação valores em lista

--- python/app.py
+++ python/app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from tkinter.commondialog import Dialog
 
@@ -13,7 +14,7 @@ class app():
         selectionMenu = Label(main,background='#121212')
         selectionMenu.pack()
         options = ["Adicionar Item","Pesquisar Item","Remover Item"]
-        dropdownMenu = ttk.Combobox(selectionMenu,textvariable='Selecione uma opção')
+        dropdownMenu = ttk.Combobox(selectionMenu,textvariable='Selecione uma opção',width=30)
         dropdownMenu['values'] = options
         dropdownMenu['state'] = 'readonly'
         dropdownMenu.set('Selecione uma opção')
@@ -25,6 +26,17 @@ class app():
         buttonSearchItem = Button(selectionMenu,image=searchImage,background='#121212')
         buttonSearchItem.image = searchImage
         buttonSearchItem.pack(side=RIGHT,padx=10)
+        labelDisplayMenu = tk.Label(main,pady=20)
+        labelDisplayMenu.pack()
+        scrollbar = tk.Scrollbar(labelDisplayMenu)
+        scrollbar.pack(side=RIGHT,fill=Y)
+        menuList = ['a','b','c']
+        menu = tk.Listbox(labelDisplayMenu,yscrollcommand=scrollbar.set)
+        menu.pack(side=LEFT)
+        for i in range(1,40):
+            menu.insert(END,str(i))
+        scrollbar.config(command=menu.yview)
+        
         
         
     
