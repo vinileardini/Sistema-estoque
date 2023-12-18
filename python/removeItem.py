61c1af5963ac3d873ac8276c16a51c10463673f5
Vinícius <vinileardini@gmail.com>
2023-12-18 11:56:27 -0300

    layout menu de remoção item

--- python/removeItem.py
+++ python/removeItem.py
@@ -12,11 +12,26 @@ class removeItem():
         labelItem = Label(mainRemoveItem,background='#040f23')
         labelItem.pack(pady=20)
         itemField = Label(labelItem,text="Item:",background='#040f23',foreground='#ffffff')
-        itemField.pack(pady=20,padx=15,side=LEFT)
+        itemField.pack(pady=20,side=LEFT)
         itemInput = Entry(labelItem,width=30)
-        itemInput.pack(pady=20)
+        itemInput.pack(pady=20,side=LEFT,padx=10)
+        
+        searchImage = PhotoImage(file='img/lupa.png')
+        searchImage = searchImage.subsample(30,30)
+        buttonSearchItem = Button(labelItem,image=searchImage,background='#040f23')
+        buttonSearchItem.image = searchImage
+        buttonSearchItem.pack(side=RIGHT)
+        
         infoLabel = Label(mainRemoveItem,background='#040d23')
-        infoLabel.pack(pady=40)
+        infoLabel.pack(pady=10)
+        # Linha tracejada
+        canvas = Canvas(infoLabel,background='#040d23',width=500,height=10,highlightthickness=0)
+        canvas.create_line(500,5,10,5,dash=(8,2),fill='#ffffff')
+        canvas.pack()
+        
+        # Display de informações do item pesquisado
+        infoField = Label(infoLabel,background='#040d23',foreground='#ffffff',text='Informações do item',font=('Arial',14))
+        infoField.pack(pady=20)
         labelId = Label(infoLabel,background='#040d23',foreground='#ffffff')
         labelId.pack(pady=10)
         idField =  Label(labelId,text='ID:',background='#040d23',foreground='#ffffff')
@@ -35,6 +50,13 @@ class removeItem():
         localField.pack(side=LEFT)
         localLabel = Label(labelLocal,background='#040d23',foreground='#ffffff',text='teste')
         localLabel.pack(side=RIGHT)
+        labelButtons = Label(infoLabel,background='#040d23',foreground='#ffffff')
+        labelButtons.pack(pady=20)
+        confirmButton = Button(labelButtons,text='✔ Confirmar',background='#02c202')
+        confirmButton.pack(padx=10,side=LEFT)
+        cancelButton = Button(labelButtons,text='❌ Excluir',background='#eb1313')
+        cancelButton.pack()
+        
         
         
         
@@ -42,8 +64,8 @@ class removeItem():
 
 root = Tk()
 root.config(background='#040f23')
-root.maxsize(300,400)
-root.minsize(300,400)
+root.maxsize(300,500)
+root.minsize(300,500)
 root.title('Remoção de item')
 imgIcon = Image.open('img\\boxcrate.png')
 icon = ImageTk.PhotoImage(imgIcon)
